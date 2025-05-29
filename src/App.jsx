4ab905874c199a02c9db3f43dfc2d964b885268b
anasousa1965 <cleideana520@gmail.com>
2025-05-29 14:13:11 +0000

    Alteração da aula 23/05/25

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,31 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
    export default function App() {
-   const texts = {
-    { title: "Meu título 1", text; "meu título 1"}
-    { title: "Meu título 2", text; "meu título 2"}
-    { title: "Meu título 3", text; "meu título 3"}
-    { title: "Meu título 4", text; "meu título 4"}
-    { title: "Meu título 5", text; "meu título 5"}
+   const texts = [
+    { title: "Meu título 1", text: "Meu título 1"},
+    { title: "Meu título 2", text: "Meu título 2"},
+    { title: "Meu título 3", text: "Meu título 3"},
+    { title: "Meu título 4", text: "Meu título 4"},
+    { title: "Meu título 5", text: "Meu título 5"},
 
-   }
+   ]
 
     return (
     // React Fragment 
     <>
-    {texts.map((text,index)) =>}
-     return (
-      <MyText key= {index} title={'$(text.title)$(index + 1)'}>
-      {text.text}
-     </MyText> 
-     ) 
-    "
-    />
+    
+    {texts.map((text, index) => {
+        return (
+          <MyText key={index} title={`${text.title} ${index + 1}`}>
+            {text.text}
+          </MyText>
+        );
+      })}
+    </>
     );
+   }
+    
+    
     
 
 
--- src/styles/components/MyText.jsx
+++ src/styles/components/MyText.jsx
@@ -1,17 +1,16 @@
 import styles from './MyText.module.css'
 
-export function MyText({title, children}) {
+export function MyText({ title, children }) {
     return (
     <div className={styles.container}>
         <div className={styles.div}>
-        <h1 className={styles.title}>{props.title}</h1>
-        <p className={styles.text}{children}>
-            {props.text}
-        </p>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{children}</p>
         </div>
         </div>
     );
-}  
+}
+        
 
     
     
