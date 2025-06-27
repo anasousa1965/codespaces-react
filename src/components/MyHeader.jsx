4c70d09eef9be90a6c8c8209c168a6d0c62d22aa
anasousa1965 <cleideana520@gmail.com>
2025-06-27 00:01:23 +0000

    Desafio 4 TRJ atualizado

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,20 +1,19 @@
-import styles from "./MyHeader.module.css";
-
-import "./App.css";
-
-export function Header() {
 export function MyHeader() {
   return (
-    <header className="header">
-      <img src="https://picsum.photos/50" alt="Ícone" />
-      <div className="logo">
+        <header className="header">
+           <div className="logo">
         <img src="https://picsum.photos/50" alt="logo" />
         <img src="https://picsum.photos/50" alt="logo" />
       </div>
       <h1>Foco, Força, Fé</h1>
     </header>
   );
-} 
+  }
 
-    
-}
+
+
+
+
+
+
+
